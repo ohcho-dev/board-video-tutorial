648e0798b69ddc229505b9090eb2639bc7b7178f
ohcho-dev <ohcho.dev@gmail.com>
2024-03-02 01:28:03 +0900

    feat: add auth render logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button size="sm" variant="destructive">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
