207bb470ab71be50d32c333d28f012a71564e622
ohcho-dev <ohcho.dev@gmail.com>
2024-03-06 17:39:40 +0900

    fix: solved export error

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,4 +1,4 @@
-export default {
+const auth = {
   providers: [
     {
       domain: "https://quality-glider-14.clerk.accounts.dev",
@@ -6,3 +6,4 @@ export default {
     },
   ],
 };
+export default auth;
