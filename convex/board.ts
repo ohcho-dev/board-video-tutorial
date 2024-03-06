1529b2270190cb610018fac294093709421dce7d
ohcho-dev <ohcho.dev@gmail.com>
2024-03-06 18:28:40 +0900

    fix: remove board with favorite data

--- convex/board.ts
+++ convex/board.ts
@@ -46,7 +46,19 @@ export const remove = mutation({
     if (!identity) {
       throw new Error("Unauthorized");
     }
-    // TODO: Later check to delete favorite relation as well
+
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
   },
@@ -97,8 +109,8 @@ export const favorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q.eq("userId", userId).eq("boardId", board._id).eq("orgId", args.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
@@ -135,10 +147,8 @@ export const unfavorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex(
-        "by_user_board_org",
-        (q) => q.eq("userId", userId).eq("boardId", board._id)
-        // TODO: check if orgId needed
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
