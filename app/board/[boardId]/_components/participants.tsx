87d97459ef34e4f16107c964e26224cb014286a4
ohcho-dev <ohcho.dev@gmail.com>
2024-03-08 09:39:16 +0900

    feat: update participants component

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,60 @@
+"use client";
+
+import { connectionIdToColor } from "@/lib/utils";
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user_avatar";
+import { useEffect, useState } from "react";
+import { ChevronLeft } from "lucide-react";
+
 export const Participants = () => {
+  const [toggle, setToggle] = useState(false);
+  const [maxShownUsers, setMaxShownUsers] = useState(1);
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > maxShownUsers;
+
+  useEffect(() => {
+    toggle ? setMaxShownUsers(users.length) : setMaxShownUsers(1);
+  }, [toggle, users.length]);
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      List of users
+      <div className="flex gap-x-2">
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (YOU)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+
+        {users.slice(0, maxShownUsers).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "T"}
+            />
+          );
+        })}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - maxShownUsers} more`}
+            fallback={`+${users.length - maxShownUsers}`}
+            onClick={() => setToggle((prev) => !prev)}
+            style={{ cursor: "pointer" }}
+          />
+        )}
+        {toggle && (
+          <div className="h-8 w-8 border-2 flex items-center justify-center rounded-full cursor-pointer">
+            <ChevronLeft onClick={() => setToggle(false)} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
