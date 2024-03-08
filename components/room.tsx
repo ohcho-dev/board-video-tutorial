01dbdaee5fddcf027377785e5b3006ef7a6e95c3
ohcho-dev <ohcho.dev@gmail.com>
2024-03-08 13:33:46 +0900

    feat: add insert layer function with rectangle

--- components/room.tsx
+++ components/room.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from "react";
 import { ClientSideSuspense } from "@liveblocks/react";
 
 import { RoomProvider } from "@/liveblocks.config";
+import { LiveMap, LiveList, LiveObject } from "@liveblocks/client";
+
+import { Layer } from "@/types/canvas";
 
 interface RoomProps {
   children: ReactNode;
@@ -17,6 +20,11 @@ export const Room = ({ children, roomId, fallback }: RoomProps) => {
       id={roomId}
       initialPresence={{
         cursor: null,
+        selection: [],
+      }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
       }}
     >
       <ClientSideSuspense fallback={fallback}>
